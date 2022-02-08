608dc924253252b700eaec785ad4be8cf90bee72
nickmerrett <nick.merrett@ibm.com>
2022-02-08 21:08:07 +1100

    vsphere deployment updates

--- clusters/cluster-build/vsphere/make_secret-vsphere.sh
+++ clusters/cluster-build/vsphere/make_secret-vsphere.sh
@@ -11,17 +11,17 @@ if [[ -z ${VSPH_USER} ]]; then
 fi
 
 if [[ -z ${SSH_PRIV} ]]; then
-  echo "Please provide environment variable SSH_PRIV_FILE"
+  echo "Please provide environment variable SSH_PRIV"
   exit 1
 fi
 
 if [[ -z ${SSH_PUB} ]]; then
-  echo "Please provide environment variable SSH_PUB_FILE, containing the path to matching public ssh key matched to the private key"
+  echo "Please provide environment variable SSH_PUB, the matching public ssh key matched to the private key"
   exit 1
 fi
 
 if [[ -z ${VSPH_CACERT} ]]; then
-  echo "Please provide environment variable VSPH_CACERT_FILE, containing the path to matching public certificate for the vcenter server"
+  echo "Please provide environment variable VSPH_CACERT, the public certificate for the vcenter server"
   exit 1
 fi
 
@@ -49,14 +49,15 @@ rm templates/install-config.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
 echo "Creating sealed secrets"
-ENC_VSPH_USER=$(echo -n ${VSPH_USER} | kubeseal --raw --name=$CLUSTER_NAME-vsphere-creds --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
-ENC_VSPH_PASS=$(echo -n ${VSPH_PASS} | kubeseal --raw --name=$CLUSTER_NAME-vsphere-creds --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
-ENC_PULL_SECRET=$(echo -n ${PULL_SECRET} | kubeseal --raw --name=$CLUSTER_NAME-pull-secret --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
-ENC_SSH_PRIV=$(echo -n ${SSH_PRIV} | kubeseal --raw --name=$CLUSTER_NAME-ssh-private-key --namespace=$CLUSTER_NAME  --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
-ENC_SSH_PUB=$(echo -n ${SSH_PUB} | kubeseal --raw --name=$CLUSTER_NAME-ssh-private-key --namespace=$CLUSTER_NAME  --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
-ENC_VSPH_PUB_CA=$(echo -n ${VSPH_CACERT} | kubeseal --raw --name=$CLUSTER_NAME-vsphere-certs --namespace=$CLUSTER_NAME  --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_VSPH_USER=$(echo -n "${VSPH_USER}" | kubeseal --raw --name=$CLUSTER_NAME-vsphere-creds --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_VSPH_PASS=$(echo -n "${VSPH_PASS}" | kubeseal --raw --name=$CLUSTER_NAME-vsphere-creds --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_PULL_SECRET=$(echo -n "${PULL_SECRET}" | kubeseal --raw --name=$CLUSTER_NAME-pull-secret --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_SSH_PRIV=$(echo -n "${SSH_PRIV}" | kubeseal --raw --name=$CLUSTER_NAME-ssh-private-key --namespace=$CLUSTER_NAME  --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_SSH_PUB=$(echo -n "${SSH_PUB}" | kubeseal --raw --name=$CLUSTER_NAME-ssh-private-key --namespace=$CLUSTER_NAME  --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_VSPH_PUB_CA=$(echo -n "${VSPH_CACERT}" | kubeseal --raw --name=$CLUSTER_NAME-vsphere-certs --namespace=$CLUSTER_NAME  --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
 
 echo "Updating values file with encrypted secrets"
+sed -i '' -e 's#.*cluster:.*$#cluster: '$CLUSTER_NAME'#g' values.yaml
 sed -i '' -e 's#.*username.*$#    username: '$ENC_VSPH_USER'#g' values.yaml
 sed -i '' -e 's#.*password.*$#    password: '$ENC_VSPH_PASS'#g' values.yaml
 sed -i '' -e 's#.*pullSecret.*$#    pullSecret: '$ENC_PULL_SECRET'#g' values.yaml
@@ -64,3 +65,7 @@ sed -i '' -e 's#.*sshPrivatekey.*$#    sshPrivatekey: '$ENC_SSH_PRIV'#g' values.
 sed -i '' -e 's#.*sshPublickey.*$#    sshPublickey: '$ENC_SSH_PUB'#g' values.yaml
 sed -i '' -e 's#.*installConfig.*$#    installConfig: '$ENC_INST_CFG'#g' values.yaml
 sed -i '' -e 's#.*cacertificate.*$#    cacertificate: '$ENC_VSPH_PUB_CA'#g' values.yaml
+
+#display output at end
+echo "--------"
+cat values.yaml
